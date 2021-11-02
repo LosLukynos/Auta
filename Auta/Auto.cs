093a2a6e3472eeb60fbe4573ac9de91c335ff27d
Lukáš Macháček <Machacek.Lukas@stud.vassboskovice.cz>
2021-11-02 13:17:04 +0100

    Vytvoření třídy Nalozit

---
+++ Auta/Auto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auta
+{
+    public class Auto
+    {
+        private int naklad;
+
+        public int Naklad { get => naklad; }
+        public string Znacka { get; }
+        public int Nosnost { get;}
+
+
+        public Auto(string znacka, int nosnost)
+        {
+            Znacka = znacka;
+            Nosnost = nosnost;
+        }
+
+        public int Nalozit(int naklad)
+        { //↓↓↓ zjistí se, kolik je tam místa, když by hrozilo přeložení, tak se naloží do plna. S tím, že zbytek se nenaloží ↓↓↓ 
+            var misto = Nosnost - Naklad;
+             
+            if (misto < naklad)
+            {
+                this.naklad += misto;
+                return misto;
+            }
+            else
+            {
+                this.naklad += naklad;     
+                return naklad;
+            }
+            
+        }
+
+        public int Vylozit(int naklad)
+        {
+
+        }
+    }
+}
